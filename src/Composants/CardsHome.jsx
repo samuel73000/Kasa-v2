8b7ba62455ba66ae416f7255f9050f47e114aafa
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2024-09-18 19:33:54 +0200

    kasa-v2

---
+++ src/Composants/CardsHome.jsx
@@ -0,0 +1,9 @@
+import "../Style/Composants/CardsHome.css"
+export default function CardsHome (){
+    return(
+        <div className="container-cards-home">
+          <img src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg" alt="pde" className="img-card-home" />
+          <p className="texte-cards-home">Appartement cosy</p>
+        </div>
+    )
+}
--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,13 +1,32 @@
- import '../Style/Home.css'
- import BannierHome from "../Assets/Bannier-page-home.png"; 
- 
- export default function Home(){
-    return(
-        <section>
-        <div className='container-bannier-home'>
-            <img src={BannierHome} alt="bannier du site" className='img-bannier-home' />
-          <h1 className='titre-bannier-home'>Chez vous, partout et ailleurs</h1>  
-        </div>
-        </section>
-    )
- }
+import "../Style/Home.css";
+import BannierHome from "../Assets/Bannier-page-home.png";
+import CardsHome from "../Composants/CardsHome";
+export default function Home() {
+  return (
+    <section>
+      {/* section bannier */}
+      <div className="container-bannier-home">
+        <img
+          src={BannierHome}
+          alt="bannier du site"
+          className="img-bannier-home"
+        />
+        <h1 className="titre-bannier-home">Chez vous, partout et ailleurs</h1>
+      </div>
+      {/* section des cards */}
+      <div className="container-all-cards-home">
+
+        <CardsHome />
+        <CardsHome />
+        <CardsHome />
+        <CardsHome />
+        <CardsHome />
+        
+
+
+
+
+      </div>
+    </section>
+  );
+}
