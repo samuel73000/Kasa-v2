485a3ec9f30a76f190a29150f9f6842d930dc579
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2024-09-19 03:03:46 +0200

    kasa-v2

--- src/Composants/CardsHome.jsx
+++ src/Composants/CardsHome.jsx
@@ -1,8 +1,11 @@
 import "../Style/Composants/CardsHome.css"
+import { Link } from "react-router-dom";
 export default function CardsHome (props){
     return(
         <div className="container-cards-home">
+            <Link to="#">
           <img src={props.lien} alt={props.alt} className="img-card-home" />
+          </Link>
           <p className="texte-cards-home">{props.titreCards}</p>
         </div>
     )
--- src/Composants/Footer.jsx
+++ src/Composants/Footer.jsx
@@ -1,8 +1,10 @@
 import "../Style/Composants/Footer.css";
+import logoFooter from "../Assets/Logo-footer-pc.png"
 export default function Footer (){
     return(
-    <footer>
-
+    <footer className="container-footer">
+<img src={logoFooter} alt="logo du site version footer"  className="img-footer"/>
+<p className="text-footer">© 2020 Kasa. All rights reserved</p>
     </footer>
     )
 }
