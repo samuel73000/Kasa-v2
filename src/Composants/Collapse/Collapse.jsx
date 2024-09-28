31082abbd28cb3b1d54961a6d41b52e64335ca11
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2024-09-28 09:59:24 +0200

    kasa-v2

--- src/Composants/Collapse/Collapse.jsx
+++ src/Composants/Collapse/Collapse.jsx
@@ -1,25 +1,26 @@
-import "./_Collapse.scss"
-import arrow from "../../Assets/arrow_back.png"
+import "./_Collapse.scss";
+import arrow from "../../Assets/arrow_back.png";
 import React, { useState } from "react";
-export default function Collapse ( props){
+export default function Collapse(props) {
+  const [open, setOpen] = useState(false);
 
-const [open , setOpen] = useState(false);
-
-    return(
-        
-       <section className="section-collapse">
-        <div className="div-titre-collapse">
+  return (
+    <section   className="section-collapse">
+      <div className="div-titre-collapse">
         <h3 className="titre-collapse">{props.titre}</h3>
-        <img onClick={() => setOpen(!open)} src={arrow} alt="fleche pour ouvire le collapse" className="arrow-collapse" />
-        </div>
-         
-        {open && (
-        <div className="div-contenu-collapse" >
-        <p className="contenu-collapse">
-         {props.contenu}
-        </p>
-        </div>
-       )}
-        </section>
-    )
+        <img
+          onClick={() => setOpen(!open)}
+          src={arrow}
+          alt="fleche pour ouvire le collapse"
+          className={`arrow-collapse  ${open ? 'arrow-collapse-active' : 'arrow-collapse-reverse'}`}
+        />
+      </div>
+
+      <div 
+  className={`div-contenu-collapse ${open ? 'div-collapse-active' : 'div-collapse-reverse'}`}
+>
+  <p className="contenu-collapse">{props.contenu}</p>
+</div>
+    </section>
+  );
 }
