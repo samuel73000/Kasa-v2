d752937bcfe25989b6524a9ff610ed0ee87a1fba
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2024-09-29 07:20:02 +0200

    kasa-v2

--- src/Composants/Slideshow/Slideshow.jsx
+++ src/Composants/Slideshow/Slideshow.jsx
@@ -1,20 +1,35 @@
 import "./_Slideshow.scss";
-// import { useData } from "../../Composants/DataFetcher";
+import { useData } from "../../Composants/DataFetcher"; // Récupère les données des logements
+import { useParams } from 'react-router-dom'; // Récupère l'ID depuis l'URL
+import React, { useState } from "react";
 import arrow_left from "../../Assets/arrow_Left.png";
 import arrow_right from "../../Assets/arrow_right.png";
+
+// Composant Slideshow
 export default function Slideshow() {
-//   const data = useData();
+  const data = useData(); // Charge les données du logement
+  const { id } = useParams(); // Récupère l'ID du logement depuis l'URL
+
+  const logement = data.find(item => item.id === id); // Trouve le logement correspondant à l'ID
+
+  let [index, setIndex] = useState(0); // Gère l'index pour naviguer entre les images
+
+  // Fonction pour aller à l'image suivante
+  function suivant() {
+    setIndex(index < logement.pictures.length-1 ? index + 1 : 0); // Incrémente ou revient à 0
+  }
+
+  // Fonction pour revenir à l'image précédente
+  function precedent() {
+    setIndex(index > 0 ? index - 1 : 4); // Décrémente ou revient à 4
+  }
 
   return (
     <section className="container-Slideshow">
-      <img className="img-fleche-Slideshow-left" src={arrow_left} alt="fleche" />
-      <img
-        className="img-logement-Slideshow"
-        src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg"
-        alt="logoment"
-      />
-      <img className="img-fleche-Slideshow-right" src={arrow_right} alt="fleche" />
-      <p className="compteur-slideshow">1/4</p>
+      <img className="img-fleche-Slideshow-left" src={arrow_left} alt="fleche" onClick={precedent} />
+      <img className="img-logement-Slideshow" src={logement.pictures[index]} alt="logement" /> {/* Affiche l'image selon l'index */}
+      <img className="img-fleche-Slideshow-right" src={arrow_right} alt="fleche" onClick={suivant}/>
+      <p className="compteur-slideshow">{index+1}/{logement.pictures.length}</p> {/* Affiche le compteur d'images */}
     </section>
   );
 }
