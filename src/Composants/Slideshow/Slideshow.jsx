9e571693d7089058e36684fa7a56384ace537711
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2024-09-29 07:59:37 +0200

    kasa-v2

--- src/Composants/Slideshow/Slideshow.jsx
+++ src/Composants/Slideshow/Slideshow.jsx
@@ -1,6 +1,6 @@
 import "./_Slideshow.scss";
-import { useData } from "../../Composants/DataFetcher"; // Récupère les données des logements
-import { useParams } from 'react-router-dom'; // Récupère l'ID depuis l'URL
+import { useData } from "../../Composants/DataFetcher";
+import { useParams } from "react-router-dom"; // Récupère l'ID depuis l'URL
 import React, { useState } from "react";
 import arrow_left from "../../Assets/arrow_Left.png";
 import arrow_right from "../../Assets/arrow_right.png";
@@ -10,13 +10,13 @@ export default function Slideshow() {
   const data = useData(); // Charge les données du logement
   const { id } = useParams(); // Récupère l'ID du logement depuis l'URL
 
-  const logement = data.find(item => item.id === id); // Trouve le logement correspondant à l'ID
+  const logement = data.find((item) => item.id === id); // Trouve le logement correspondant à l'ID
 
   let [index, setIndex] = useState(0); // Gère l'index pour naviguer entre les images
 
   // Fonction pour aller à l'image suivante
   function suivant() {
-    setIndex(index < logement.pictures.length-1 ? index + 1 : 0); // Incrémente ou revient à 0
+    setIndex(index < logement.pictures.length - 1 ? index + 1 : 0); // Incrémente ou revient à 0
   }
 
   // Fonction pour revenir à l'image précédente
@@ -26,10 +26,28 @@ export default function Slideshow() {
 
   return (
     <section className="container-Slideshow">
-      <img className="img-fleche-Slideshow-left" src={arrow_left} alt="fleche" onClick={precedent} />
-      <img className="img-logement-Slideshow" src={logement.pictures[index]} alt="logement" /> {/* Affiche l'image selon l'index */}
-      <img className="img-fleche-Slideshow-right" src={arrow_right} alt="fleche" onClick={suivant}/>
-      <p className="compteur-slideshow">{index+1}/{logement.pictures.length}</p> {/* Affiche le compteur d'images */}
+      <img
+        className="img-fleche-Slideshow-left"
+        src={arrow_left}
+        alt="fleche"
+        onClick={precedent}
+      />
+      <img
+        className="img-logement-Slideshow"
+        src={logement.pictures[index]}
+        alt="logement"
+      />{" "}
+      {/* Affiche l'image selon l'index */}
+      <img
+        className="img-fleche-Slideshow-right"
+        src={arrow_right}
+        alt="fleche"
+        onClick={suivant}
+      />
+      <p className="compteur-slideshow">
+        {index + 1}/{logement.pictures.length}
+      </p>{" "}
+      {/* Affiche le compteur d'images */}
     </section>
   );
 }
--- src/Page/Logements/Logements.jsx
+++ src/Page/Logements/Logements.jsx
@@ -1,10 +1,25 @@
-import "./_Logements.scss"
-import Slideshow from "../../Composants/Slideshow/Slideshow"
-export default function Logement (){
+import "./_Logements.scss";
+import { useData } from "../../Composants/DataFetcher";
+import { useParams } from "react-router-dom"; // Récupère l'ID depuis l'URL
+import Slideshow from "../../Composants/Slideshow/Slideshow";
+export default function Logement() {
+  const data = useData(); // Charge les données du logement
+  const { id } = useParams(); // Récupère l'ID du logement depuis l'URL
 
-    return (
-        <section>
-        <Slideshow />
-        </section>
-    )
+  const logement = data.find((item) => item.id === id); // Trouve le logement correspondant à l'ID
+  return (
+    <section>
+      <Slideshow />
+      <div>
+        <div>
+          <h1>{logement.title}</h1>
+          <p>{logement.location}</p>
+        </div>
+        <div>
+          <p>{logement.host.name}</p>
+          <img src={logement.host.picture} alt="" />
+        </div>
+      </div>
+    </section>
+  );
 }
