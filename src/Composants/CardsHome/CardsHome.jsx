fa4f81d1e08add36012ff90c4006aa1d9d7afd06
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2024-10-05 09:20:50 +0200

    kasa

--- src/Composants/CardsHome/CardsHome.jsx
+++ src/Composants/CardsHome/CardsHome.jsx
@@ -1,17 +1,15 @@
-import "./_CardsHome.scss"
+import "./_CardsHome.scss";
 import { Link } from "react-router-dom";
 
-export default function CardsHome (props){
-
-
-    return(
-        <div className="container-cards-home" id={props.id}>
-      <Link to={`/logement/${props.id}`}> {/* Utilisation du chemin dynamique */}
-      <img src={props.lien} alt={props.alt} className="img-card-home" />
-          </Link>
-          <p className="texte-cards-home">{props.titreCards}</p>
-        </div>
-    )
+export default function CardsHome(props) {
+  return (
+    <div className="container-cards-home" id={props.id}>
+      <Link to={`/logement/${props.id}`}>
+        {" "}
+        {/* Utilisation du chemin dynamique */}
+        <img src={props.lien} alt={props.alt} className="img-card-home" />
+      </Link>
+      <p className="texte-cards-home">{props.titreCards}</p>
+    </div>
+  );
 }
-
-
